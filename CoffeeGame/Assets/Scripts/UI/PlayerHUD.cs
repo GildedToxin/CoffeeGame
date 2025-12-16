c7bc3633dfdc7b30d0aa475b38032b602f6e5182
Toxin4426 <Toxin4426@gmail.com>
2025-12-16 01:56:00 -0500

    Create Health Bar

--- CoffeeGame/Assets/Scripts/UI/PlayerHUD.cs
+++ CoffeeGame/Assets/Scripts/UI/PlayerHUD.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class PlayerHUD : MonoBehaviour
 {
     private PlayerController player;
+    public GameObject HealthBar;
 
     void Start()
     {
+        HealthBar.GetComponent<UnityEngine.UI.Slider>().value = 1;
         player.OnHealthChanged += UpdateHealth;
     }
 
     void UpdateHealth(float current, float max)
     {
+        HealthBar.GetComponent<UnityEngine.UI.Slider>().value = current / max;
     }
 
     void OnDestroy()
